ad126bb9b4446d9709104d31d390416e0de279fe
gerrard088 <positive4163@gmail.com>
2026-01-23 22:19:39 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -1,29 +1,23 @@
 #include <iostream>
+#include <vector>
 #include <string>
-
 using namespace std;
 
 int main() {
-    int T;
-    cin >> T;
 
-    while (T--) { // T번 반복
-        int R;
-        string S;
-        cin >> R >> S;
+	int testnum;
+	cin >> testnum;
 
-        // 1. 문자열 S의 각 문자를 하나씩 선택 (바깥쪽 루프)
-        for (int i = 0; i < S.length(); i++) {
+	for (int q = 0; q < testnum; q++) {
+		int repeat;
+		string name;
+		cin >> repeat>>name;
 
-            // 2. 해당 문자를 R번 반복해서 출력 (안쪽 루프)
-            for (int j = 0; j < R; j++) {
-                cout << S[i];
-            }
-        }
-
-        // 3. 한 테스트 케이스가 끝나면 줄바꿈
-        cout << endl;
-    }
-
-    return 0;
+		for (int w = 0; w < name.length(); w++) {
+			for (int q = 0; q < repeat; q++) {
+				cout << name[w];
+			}
+		}
+		cout << endl;
+	}
 }
