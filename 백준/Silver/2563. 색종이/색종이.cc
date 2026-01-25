8c342639feae0e11387edd0f5e13a579f4b3b7b7
gerrard088 <positive4163@gmail.com>
2026-01-25 20:07:21 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
 	
-	vector<vector<int>>paper(101, vector<int>(101));
+	vector<vector<int>>paper(100, vector<int>(100));
 
 	int num;
 	cin >> num;
@@ -16,15 +16,15 @@ int main() {
 	int x, y;
 	for (int q = 0; q < num; q++) {
 		cin >> x >> y;
-		for (int w = x+1; w < x+11; w++) {
-			for (int e = y+1; e < y+11; e++) {
+		for (int w = x; w < x+10; w++) {
+			for (int e = y; e < y+10; e++) {
 				paper[w][e] = 1;
 			}
 		}
 	}
 	int total = 0;
-	for (int r = 0; r < 101; r++) {
-		for (int t = 0; t < 101; t++) {
+	for (int r = 0; r < 100; r++) {
+		for (int t = 0; t < 100; t++) {
 			if (paper[r][t] != 0) { total += 1; }
 		}
 	}
