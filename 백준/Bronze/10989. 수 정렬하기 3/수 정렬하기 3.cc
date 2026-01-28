fa82fab2ab5e922a4999c6ee96c2645f56f46ab7
gerrard088 <positive4163@gmail.com>
2026-01-28 20:36:40 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1516 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,30 +1,30 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
-// 💡 전역 변수로 선언하여 메모리를 안정적으로 할당합니다.
-int cnt[10001] = {0};
+
+vector<int>arr(10001);
 
 int main() {
-    // 입출력 가속기 (천만 개의 데이터이므로 필수!)
+    
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin >> n;
+    int num;
+    cin >> num;
 
-    // 1. 숫자를 입력받을 때마다 해당 숫자의 카운트를 증가시킵니다.
-    for (int i = 0; i < n; i++) {
-        int num;
-        cin >> num;
-        cnt[num]++;
+
+    int input;
+    for (int q = 0; q < num; q++) {
+        cin >> input;
+        arr[input] += 1;
     }
 
-    // 2. 1부터 10,000까지 돌면서 카운트된 횟수만큼 숫자를 출력합니다.
-    for (int i = 1; i <= 10000; i++) {
-        if (cnt[i] != 0) {
-            for (int j = 0; j < cnt[i]; j++) {
-                cout << i << "\n";
+
+    for (int w = 0; w < 10001; w++) {
+        if (arr[w] != 0) {
+            for (int e = 0; e < arr[w]; e++) {
+                cout << w << "\n";
             }
         }
     }
