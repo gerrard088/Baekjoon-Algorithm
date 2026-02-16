38d64197665d2b792224df8e3c3078c70b6fc88b
gerrard088 <positive4163@gmail.com>
2026-02-17 00:45:02 +0900

    Update 수 찾기.cc

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,49 +1,35 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 int main() {
-    // 10만 개의 데이터를 빠르게 입력받기 위한 필수 코드
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
 
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; i++) cin >> A[i];
+	int N, M;
+	cin >> N;
 
-    // 💡 이분 탐색의 전제 조건: 반드시 정렬되어 있어야 함!
-    sort(A.begin(), A.end());
+	vector<int>list(N);
+	for (int q = 0; q < N; q++) {
+		cin >> list[q];
+	}
 
-    int M;
-    cin >> M;
-    while (M--) {
-        int target;
-        cin >> target;
+	cin >> M;
+	vector<int>goal(M);
+	for (int q = 0; q < M; q++) {
+		cin >> goal[q];
+	}
 
-        int left = 0;
-        int right = N - 1;
-        bool found = false;
+	sort(list.begin(), list.end());
+	list.erase(unique(list.begin(), list.end()), list.end());
 
-        // 이분 탐색 시작
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+	for (int q = 0; q < M; q++) {
+		if (binary_search(list.begin(), list.end(), goal[q])) {
+			cout << 1 << '\n';
+		}
+		else
+			cout << 0 << '\n';
+	}
 
-            if (A[mid] == target) {
-                found = true;
-                break;
-            } else if (A[mid] < target) {
-                left = mid + 1; // 오른쪽 구간 탐색
-            } else {
-                right = mid - 1; // 왼쪽 구간 탐색
-            }
-        }
 
-        if (found) cout << "1\n";
-        else cout << "0\n";
-    }
-
-    return 0;
+	return 0;
 }
