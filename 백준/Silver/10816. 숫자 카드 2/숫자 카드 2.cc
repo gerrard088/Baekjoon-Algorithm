3b8969bbb7e5a0d8cd63fb321fb9f7d3a4ea0666
gerrard088 <positive4163@gmail.com>
2026-02-19 21:32:03 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 300 ms, Memory: 3976 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <string>
 
 using namespace std;
 
 int main() {
+    // 입출력 속도 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int N, M;
-	cin >> N;
+    int N;
+    cin >> N;
+    vector<int> cards(N);
+    for (int i = 0; i < N; i++) {
+        cin >> cards[i];
+    }
 
-	vector<int>arrN(N);
-	for (int q = 0; q < N; q++) {
-		cin >> arrN[q];
-	}
+    // 1. 이분 탐색을 위해 오름차순 정렬 (O(N log N))
+    sort(cards.begin(), cards.end());
 
-	cin >> M;
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int target;
+        cin >> target;
 
-	vector<int>arrM(M);
-	for (int q = 0; q < M; q++) {
-		cin >> arrM[q];
-	}
+        // 2. upper_bound - lower_bound로 개수 계산 (O(M log N))
+        // target보다 큰 첫 번째 위치에서 target 이상의 첫 번째 위치를 뺍니다.
+        auto low = lower_bound(cards.begin(), cards.end(), target);
+        auto up = upper_bound(cards.begin(), cards.end(), target);
+        
+        cout << up - low << " ";
+    }
 
-	sort(arrN.begin(), arrN.end());
-
-	for (int q = 0; q < M; q++) {
-		
-		auto low = lower_bound(arrN.begin(), arrN.end(), arrM[q]);
-		auto up = upper_bound(arrN.begin(), arrN.end(), arrM[q]);
-
-		cout << up - low << ' ';
-	}
-
-	
-	return 0;
+    return 0;
 }
