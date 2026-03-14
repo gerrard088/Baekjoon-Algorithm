26aa5869f359e42e5573f2230b6453d77b9f570c
gerrard088 <positive4163@gmail.com>
2026-03-14 22:30:45 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,28 +1,26 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
 
+	int N;
+	cin >> N;
 
-    int dp[1001][3];
-    int cost[3];
+	int dp[1001][3];
+	int cost[3];
 
-    cin >> dp[0][0] >> dp[0][1] >> dp[0][2];
+	cin >> dp[0][0] >> dp[0][1] >> dp[0][2];
 
-    for (int i = 1; i < N; i++) {
-        cin >> cost[0] >> cost[1] >> cost[2];
+	for (int q = 1; q < N; q++) {
+		cin >> cost[0] >> cost[1] >> cost[2];
+		dp[q][0] = min(dp[q - 1][1], dp[q - 1][2]) + cost[0];
+		dp[q][1] = min(dp[q - 1][0], dp[q - 1][2]) + cost[1];
+		dp[q][2] = min(dp[q - 1][0], dp[q - 1][1]) + cost[2];
+	}
 
-        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[0];
-        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[1];
-        dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[2];
-    }
+	cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]});
 
-    cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]}) << "\n";
-
-    return 0;
+	return 0;
 }
