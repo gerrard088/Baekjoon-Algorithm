0264f3fb7d922922f38ab6ad610b7a3da134d390
yoonjin cho <positive4163@gmail.com>
2026-03-30 09:42:08 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<int> wine(n + 1, 0);
+    vector<int> dp(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> wine[i];
+    }
+
+    dp[1] = wine[1];
+    if (n >= 2) {
+        dp[2] = wine[1] + wine[2];
+    }
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = max(dp[i - 1], max(wine[i] + dp[i - 2], wine[i] + wine[i - 1] + dp[i - 3]));
+    }
+
+    cout << dp[n] << "\n";
+
+    return 0;
+}
